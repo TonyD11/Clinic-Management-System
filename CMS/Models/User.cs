2b24142d7b2775a3e47cc4cbb06d619935cd6bff
TonyD11 <cb015830@students.apiit.lk>
2025-05-07 15:17:30 +0530

    User Class

---
+++ CMS/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    abstract class User
+    {
+        private int id;
+        private string name;
+        private string password;
+        private string type;
+
+        protected User(string name, string password, string type)
+        {
+            Name = name;
+            Password = password;
+            Type = type;
+        }
+
+        public int Id { get { return id; } }
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Password { get { return password; } set { password = value; } }
+        public string Type { get { return type; } set { type = value; } }
+
+    }
+}
