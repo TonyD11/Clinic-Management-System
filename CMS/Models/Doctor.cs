af0b7c93a5f9ca0fb19f6c8e521175b262858450
TonyD11 <cb015830@students.apiit.lk>
2025-05-09 03:20:09 +0530

    Doctor Model Created

---
+++ CMS/Models/Doctor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CMS.Models
+{
+    internal class Doctor : User
+    {
+        private int age;
+        private string gender;
+        private string contact;
+
+        public Doctor(string name, string password,, int age, string gender, string contact) : base(name, password)
+        {
+            this.age = age;
+            this.gender = gender;
+            this.contact = contact;
+            Type = "doctor";
+        }
+
+        public int Age { get { return age; } set { age = value; } }
+        public string Gender { get { return gender; } set { gender = value; } }
+        public string Contact { get { return contact; } set { contact = value; } }
+    }
+}
