9cdf9ed18ed2692977ff5f572e38c64b778f8629
Imthishan <Promobiler26@gmail.com>
2025-05-09 03:31:25 +0530

    Admin Dashboard movements

--- CMS/Views/AdminDashboard.cs
+++ CMS/Views/AdminDashboard.cs
@@ -16,5 +16,18 @@ namespace CMS.Views
         {
             InitializeComponent();
         }
+        public void showForm(object Form)
+        {
+            if (this.panel2.Controls.Count > 0)
+            {
+                this.panel2.Controls.RemoveAt(0);
+            }
+            Form frm = Form as Form;
+            frm.TopLevel = false;
+            frm.Dock = DockStyle.Fill;
+            this.panel2.Controls.Add(frm);
+            this.panel2.Tag = frm;
+            frm.Show();
+        }
     }
 }
