c77045e08691bd04617e1918ceb023b296014e40
sihath2006 <Sihathdesilva@gmail.com>
2025-05-10 04:44:07 +0530

    Prescription Model

--- CMS/Models/Medicine.cs
+++ CMS/Models/Medicine.cs
@@ -12,19 +12,21 @@ namespace CMS.Models
         private string _name;
         private string _dosage;
         private string _instructions;
+        private Prescription _prescription;
 
-        public Medicine(int id, string name, string dosage, string instructions)
+        public Medicine(string name, string dosage, string instructions, Prescription prescription)
         {
-            Id = id;
             Name = name;
             Dosage = dosage;
             Instructions = instructions;
+            Prescription = prescription;
         }
 
         public int Id { get { return _id; } set { _id = value; } }
         public string Name { get { return _name; } set { _name = value; } }
         public string Dosage { get { return _dosage; } set { _dosage = value; } }
         public string Instructions { get { return _instructions; } set { _instructions = value; } }
+        public Prescription Prescription { get { return  _prescription; } set { _prescription = value; } }
 
     }
 }
---
+++ CMS/Models/Prescription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    internal class Prescription
+    {
+        private int _id;
+        private Appoitments _appoitments;
+
+        public Prescription(Appoitments appoitments)
+        {
+            Appoitments = appoitments;
+        }
+
+        public int Id { get { return _id; } set { _id = value; } }
+        public Appoitments Appoitments {  get { return this._appoitments;} set { this._appoitments = value; } }
+
+    }
+}
